77c979487ca516089a58bdf3c37eaeeb0f262cf8
Atlante893 <64986754+Atlante893@users.noreply.github.com>
2022-07-18 11:02:13 +0300

    Add files via upload

--- conftest.py
+++ conftest.py
@@ -1,23 +1,25 @@
 import pytest
-from selenium.webdriver import Chrome, Edge, Firefox
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 @pytest.fixture()
 def chrome_web_test():
-    driver_chrome = Chrome("Drivers/chromedriver.exe")
+    driver_chrome = webdriver.Chrome(ChromeDriverManager().install())
     yield driver_chrome
     driver_chrome.quit()
 
 
 @pytest.fixture()
 def firefox_web_test():
-    driver_firefox = Firefox("Drivers/geckodriver.exe")
+    driver_firefox = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     yield driver_firefox
     driver_firefox.quit()
 
 
 @pytest.fixture()
 def edge_web_test():
-    driver_edge = Edge("Drivers/msedgedriver.exe")
+    driver_edge = webdriver.Edge(EdgeChromiumDriverManager().install())
     yield driver_edge
     driver_edge.quit()
